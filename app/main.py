1b51a67ec5c4836f3bb148983aac40bb8080470e
kwasniewskin <nikodem_kwasniewski@tlen.pl>
2025-05-19 23:03:25 +0200

    Add HTML UI: include index.html template, style.css static asset, configure main.py, and update requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from app.api.endpoints import router as api_router
 
 app = FastAPI(title="Prepify")
 
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
 app.include_router(api_router, prefix="/api")
 
-@app.get("/")
-def root():
-    return {"message": "Prepify działa!"}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
